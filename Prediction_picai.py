344956d70dd718cdb4691e8cc792ca042a7cd695
tayebiarasteh <sorooshta@gmail.com>
2024-11-04 13:56:20 +0100

    statistical analysis v1

--- Prediction_picai.py
+++ Prediction_picai.py
@@ -13,8 +13,8 @@ import numpy as np
 import torchmetrics
 from tqdm import tqdm
 import torch.nn.functional as F
-import torchio as tio
 import nibabel as nib
+import pandas as pd
 
 from config.serde import read_config
 
@@ -152,3 +152,304 @@ class Prediction:
 
 
         return new_label
+
+
+
+    def predict_only(self, test_loader):
+        """Evaluation with metrics epoch
+        Returns
+        -------
+        epoch_f1_score: float
+            average test F1 score
+        average_specificity: float
+            average test specificity
+        average_sensitivity: float
+            average test sensitivity
+        average_precision: float
+            average test precision
+        """
+        self.model.eval()
+        total_f1_score = []
+        total_accuracy = []
+        total_specifity_score = []
+        total_sensitivity_score = []
+        total_precision_score = []
+
+        for idx, (image, label) in enumerate(tqdm(test_loader)):
+            image = image.to(self.device)
+            label = label.to(self.device)
+
+            with torch.no_grad():
+                output = self.model(image)
+                output_sigmoided = F.sigmoid(output)
+                output_sigmoided = (output_sigmoided > 0.5).float()
+
+            ############ Evaluation metric calculation ########
+            # Metrics calculation (macro) over the whole set
+            label = label.int()
+
+            confusioner = torchmetrics.ConfusionMatrix(num_classes=output_sigmoided.shape[1], multilabel=True).to(self.device)
+            confusion = confusioner(output_sigmoided.squeeze(0,1).flatten(), label.squeeze(0).flatten())
+
+            F1_disease = []
+            accuracy_disease = []
+            specifity_disease = []
+            sensitivity_disease = []
+            precision_disease = []
+
+            for idx, disease in enumerate(confusion):
+                TN = disease[0, 0]
+                FP = disease[0, 1]
+                FN = disease[1, 0]
+                TP = disease[1, 1]
+                F1_disease.append(2 * TP / (2 * TP + FN + FP + epsilon))
+                accuracy_disease.append((TP + TN) / (TP + TN + FP + FN + epsilon))
+                specifity_disease.append(TN / (TN + FP + epsilon))
+                sensitivity_disease.append(TP / (TP + FN + epsilon))
+                precision_disease.append(TP / (TP + FP + epsilon))
+
+            # Macro averaging
+            total_f1_score.append(torch.stack(F1_disease))
+            total_accuracy.append(torch.stack(accuracy_disease))
+            total_specifity_score.append(torch.stack(specifity_disease))
+            total_sensitivity_score.append(torch.stack(sensitivity_disease))
+            total_precision_score.append(torch.stack(precision_disease))
+
+        average_f1_score = torch.stack(total_f1_score)
+        average_accuracy = torch.stack(total_accuracy)
+        average_specifity = torch.stack(total_specifity_score)
+        average_sensitivity = torch.stack(total_sensitivity_score)
+        average_precision = torch.stack(total_precision_score)
+
+        return average_f1_score.cpu().numpy(), average_accuracy.cpu().numpy(), average_specifity.cpu().numpy(), average_sensitivity.cpu().numpy(), average_precision.cpu().numpy()
+
+
+
+    def bootstrapper(self, test_F1, test_accuracy, test_specificity, test_sensitivity, test_precision, index_list):
+        self.model.eval()
+        F1_list = []
+        accuracy_list = []
+        specificity_list = []
+        sensitivity_list = []
+        precision_list = []
+
+        test_F1 = test_F1.flatten()
+        test_accuracy = test_accuracy.flatten()
+        test_specificity = test_specificity.flatten()
+        test_sensitivity = test_sensitivity.flatten()
+        test_precision = test_precision.flatten()
+
+        print('bootstrapping ... \n')
+
+        for counter in range(1000):
+
+            new_F1 = test_F1[index_list[counter]]
+            new_accuracy = test_accuracy[index_list[counter]]
+            new_specificity = test_specificity[index_list[counter]]
+            new_sensitivity = test_sensitivity[index_list[counter]]
+            new_precision = test_precision[index_list[counter]]
+
+            F1_list.append(new_F1.mean())
+            accuracy_list.append(new_accuracy.mean())
+            specificity_list.append(new_specificity.mean())
+            sensitivity_list.append(new_sensitivity.mean())
+            precision_list.append(new_precision.mean())
+
+        F1_list = np.stack(F1_list)
+        accuracy_list = np.stack(accuracy_list)
+        specificity_list = np.stack(specificity_list)
+        sensitivity_list = np.stack(sensitivity_list)
+        precision_list = np.stack(precision_list)
+
+        print('------------------------------------------------------'
+              '----------------------------------')
+        print('\t experiment:' + self.params['experiment_name'] + '\n')
+
+        print(f'\t Dice: {F1_list.mean() * 100:.2f}% ± {F1_list.std() * 100:.2f} | Accuracy: {accuracy_list.mean() * 100:.2f}% ± {accuracy_list.std() * 100:.2f}'
+              f' | Specificity: {specificity_list.mean() * 100:.2f}% ± {specificity_list.std() * 100:.2f}'
+              f' | Sensitivity (Recall): {sensitivity_list.mean() * 100:.2f}% ± {sensitivity_list.std() * 100:.2f} | Precision: {precision_list.mean() * 100:.2f}% ± {precision_list.std() * 100:.2f}\n')
+
+        print('------------------------------------------------------'
+              '----------------------------------')
+
+        # saving the stats
+        msg = f'\n\n----------------------------------------------------------------------------------------\n' \
+              '\t experiment:' + self.params['experiment_name'] + '\n\n' \
+              f'Dice: {F1_list.mean() * 100:.2f}% ± {F1_list.std() * 100:.2f} | Accuracy: {accuracy_list.mean() * 100:.2f}% ± {accuracy_list.std() * 100:.2f} ' \
+              f' | Specificity: {specificity_list.mean() * 100:.2f}% ± {specificity_list.std() * 100:.2f}' \
+              f' | Sensitivity (Recall): {sensitivity_list.mean() * 100:.2f}% ± {sensitivity_list.std() * 100:.2f} | Precision: {precision_list.mean() * 100:.2f}% ± {precision_list.std() * 100:.2f}\n\n'
+
+        with open(os.path.join(self.params['target_dir'], self.params['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+
+        df = pd.DataFrame({'Dice_mean': F1_list *100, 'Accuracy_mean': accuracy_list *100, 'Specificity_mean': specificity_list *100, 'Sensitivity_mean': sensitivity_list *100, 'Precision_mean': precision_list *100})
+
+        df.to_csv(os.path.join(self.params['target_dir'], self.params['stat_log_path']) + '/bootstrapped_results.csv', sep=',', index=False)
+
+        return F1_list, accuracy_list, specificity_list, sensitivity_list, precision_list
+
+
+
+    def pvalue_calculator(self, F1_list1, accuracy_list1, specificity_list1, sensitivity_list1, precision_list1, params1,
+                          F1_list2, accuracy_list2, specificity_list2, sensitivity_list2, precision_list2, params2):
+
+        ########################## Dice ########################
+        counter = F1_list1 > F1_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            print(f'\n\nDice p-value: {ratio1}; model 1 significantly higher Dice than model 2')
+        else:
+            counter = F1_list2 > F1_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                print(f'\n\nDice p-value: {ratio2}; model 2 significantly higher Dice than model 1')
+            else:
+                print(f'\n\nDice p-value: {ratio1}; models NOT significantly different in terms of Dice')
+
+        counter = F1_list1 > F1_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            msg = f'\n\nDice p-value: {ratio1}; model 1 significantly higher Dice than model 2'
+        else:
+            counter = F1_list2 > F1_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                msg = f'\n\nDice p-value: {ratio2}; model 2 significantly higher Dice than model 1'
+            else:
+                msg = f'\n\nDice p-value: {ratio1}; models NOT significantly different in terms of Dice'
+
+        with open(os.path.join(params1['target_dir'], params1['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        with open(os.path.join(params2['target_dir'], params2['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        ########################## Dice ########################
+
+
+        ########################## Accuracy ########################
+        counter = accuracy_list1 > accuracy_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            print(f'\n\nAccuracy p-value: {ratio1}; model 1 significantly higher Accuracy than model 2')
+        else:
+            counter = accuracy_list2 > accuracy_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                print(f'\n\nAccuracy p-value: {ratio2}; model 2 significantly higher Accuracy than model 1')
+            else:
+                print(f'\n\nAccuracy p-value: {ratio1}; models NOT significantly different in terms of Accuracy')
+
+        counter = accuracy_list1 > accuracy_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            msg = f'\n\nAccuracy p-value: {ratio1}; model 1 significantly higher Accuracy than model 2'
+        else:
+            counter = accuracy_list2 > accuracy_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                msg = f'\n\nAccuracy p-value: {ratio2}; model 2 significantly higher Accuracy than model 1'
+            else:
+                msg = f'\n\nAccuracy p-value: {ratio1}; models NOT significantly different in terms of Accuracy'
+
+        with open(os.path.join(params1['target_dir'], params1['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        with open(os.path.join(params2['target_dir'], params2['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        ########################## Accuracy ########################
+
+
+        ########################## Specificity ########################
+        counter = specificity_list1 > specificity_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            print(f'\n\nSpecificity p-value: {ratio1}; model 1 significantly higher Specificity than model 2')
+        else:
+            counter = specificity_list2 > specificity_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                print(f'\n\nSpecificity p-value: {ratio2}; model 2 significantly higher Specificity than model 1')
+            else:
+                print(f'\n\nSpecificity p-value: {ratio1}; models NOT significantly different in terms of Specificity')
+
+        counter = specificity_list1 > specificity_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            msg = f'\n\nSpecificity p-value: {ratio1}; model 1 significantly higher Specificity than model 2'
+        else:
+            counter = specificity_list2 > specificity_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                msg = f'\n\nSpecificity p-value: {ratio2}; model 2 significantly higher Specificity than model 1'
+            else:
+                msg = f'\n\nSpecificity p-value: {ratio1}; models NOT significantly different in terms of Specificity'
+
+        with open(os.path.join(params1['target_dir'], params1['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        with open(os.path.join(params2['target_dir'], params2['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        ########################## Specificity ########################
+
+
+        ########################## Sensitivity ########################
+        counter = sensitivity_list1 > sensitivity_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            print(f'\n\nSensitivity p-value: {ratio1}; model 1 significantly higher Sensitivity than model 2')
+        else:
+            counter = sensitivity_list2 > sensitivity_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                print(f'\n\nSensitivity p-value: {ratio2}; model 2 significantly higher Sensitivity than model 1')
+            else:
+                print(f'\n\nSensitivity p-value: {ratio1}; models NOT significantly different in terms of Sensitivity')
+
+        counter = sensitivity_list1 > sensitivity_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            msg = f'\n\nSensitivity p-value: {ratio1}; model 1 significantly higher Sensitivity than model 2'
+        else:
+            counter = sensitivity_list2 > sensitivity_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                msg = f'\n\nSensitivity p-value: {ratio2}; model 2 significantly higher Sensitivity than model 1'
+            else:
+                msg = f'\n\nSensitivity p-value: {ratio1}; models NOT significantly different in terms of Sensitivity'
+
+        with open(os.path.join(params1['target_dir'], params1['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        with open(os.path.join(params2['target_dir'], params2['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        ########################## Sensitivity ########################
+
+
+        ########################## Precision ########################
+        counter = precision_list1 > precision_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            print(f'\n\nPrecision p-value: {ratio1}; model 1 significantly higher Precision than model 2')
+        else:
+            counter = precision_list2 > precision_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                print(f'\n\nPrecision p-value: {ratio2}; model 2 significantly higher Precision than model 1')
+            else:
+                print(f'\n\nPrecision p-value: {ratio1}; models NOT significantly different in terms of Precision')
+
+        counter = precision_list1 > precision_list2
+        ratio1 = (len(counter) - counter.sum()) / len(counter)
+        if ratio1 <= 0.05:
+            msg = f'\n\nPrecision p-value: {ratio1}; model 1 significantly higher Precision than model 2'
+        else:
+            counter = precision_list2 > precision_list1
+            ratio2 = (len(counter) - counter.sum()) / len(counter)
+            if ratio2 <= 0.05:
+                msg = f'\n\nPrecision p-value: {ratio2}; model 2 significantly higher Precision than model 1'
+            else:
+                msg = f'\n\nPrecision p-value: {ratio1}; models NOT significantly different in terms of Precision'
+
+        with open(os.path.join(params1['target_dir'], params1['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        with open(os.path.join(params2['target_dir'], params2['stat_log_path']) + '/test_Stats', 'a') as f:
+            f.write(msg)
+        ########################## Sensitivity ########################
+
+
--- main_3D_picai.py
+++ main_3D_picai.py
@@ -32,7 +32,7 @@ epsilon = 1e-15
 
 
 def main_train_3D(global_config_path="picai-segmentation/config/config.yaml", valid=False,
-                  resume=False, augment=False, experiment_name='name'):
+                  resume=False, augment=False, experiment_name='name', modality=1, multimodal=True):
     """Main function for training + validation for directly 3d-wise
 
         Parameters
@@ -61,20 +61,23 @@ def main_train_3D(global_config_path="picai-segmentation/config/config.yaml", va
     cfg_path = params["cfg_path"]
 
     # Changeable network parameters
-    model = UNet3D(n_out_classes=1, firstdim=48)
+    if multimodal:
+        model = UNet3D(n_in_channels=3, n_out_classes=1, firstdim=48)
+    else:
+        model = UNet3D(n_in_channels=1, n_out_classes=1, firstdim=48)
     # weight = torch.Tensor(params['class_weights'])
     weight = None
 
-    # loss_function = BinaryDiceLoss
-    loss_function = torch.nn.BCELoss
+    loss_function = BinaryDiceLoss
+    # loss_function = torch.nn.BCELoss
     optimizer = torch.optim.Adam(model.parameters(), lr=float(params['Network']['lr']),
                                  weight_decay=float(params['Network']['weight_decay']), amsgrad=params['Network']['amsgrad'])
 
-    train_dataset = data_loader_3D(cfg_path=cfg_path, mode='train')
+    train_dataset = data_loader_3D(cfg_path=cfg_path, mode='train', modality=modality, multimodal=multimodal)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=params['Network']['batch_size'],
                                                pin_memory=True, drop_last=True, shuffle=True, num_workers=10)
     if valid:
-        valid_dataset = data_loader_3D(cfg_path=cfg_path, mode='valid')
+        valid_dataset = data_loader_3D(cfg_path=cfg_path, mode='valid', modality=modality, multimodal=multimodal)
         valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size=params['Network']['batch_size_testvlid'],
                                                    pin_memory=True, drop_last=True, shuffle=False, num_workers=6)
     else:
@@ -133,6 +136,73 @@ def main_evaluate_3D(global_config_path="picai-segmentation/config/config.yaml",
 
 
 
+def main_evaluate_3D_bootstrap_pvalue(global_config_path="picai-segmentation/config/config.yaml", experiment_name1='central_exp_for_test',
+                                      experiment_name2='central_exp_for_test', experiment1_epoch_num=100, experiment2_epoch_num=100, modality=1, multimodal1=True, multimodal2=True):
+    """Evaluation for all the images using the labels and calculating metrics.
+
+    Parameters
+    ----------
+    experiment_name: str
+        name of the experiment to be loaded.
+    """
+    params1 = open_experiment(experiment_name1, global_config_path)
+    cfg_path1 = params1['cfg_path']
+
+
+    # Changeable network parameters
+    if multimodal1:
+        model1 = UNet3D(n_in_channels=3, n_out_classes=1, firstdim=48)
+    else:
+        model1 = UNet3D(n_in_channels=1, n_out_classes=1, firstdim=48)
+
+    # Generate test set
+    test_dataset = data_loader_3D(cfg_path=cfg_path1, mode='test', modality=modality, multimodal=multimodal1)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=params1['Network']['batch_size_testvlid'],
+                                               pin_memory=True, drop_last=False, shuffle=False, num_workers=16)
+
+    index_list = []
+    for counter in range(1000):
+        index_list.append(np.random.choice(len(test_dataset), len(test_dataset)))
+
+    # Initialize prediction 1
+    predictor1 = Prediction(cfg_path1)
+    predictor1.setup_model(model=model1, modelepoch=experiment1_epoch_num)
+    test_F1_1, test_accuracy1, test_specificity1, test_sensitivity1, test_precision1 = predictor1.predict_only(test_loader)
+
+    F1_list1, accuracy_list1, specificity_list1, sensitivity_list1, precision_list1 = predictor1.bootstrapper(test_F1_1, test_accuracy1, test_specificity1, test_sensitivity1, test_precision1, index_list)
+
+
+    # Changeable network parameters
+    if multimodal2:
+        model2 = UNet3D(n_in_channels=3, n_out_classes=1, firstdim=48)
+    else:
+        model2 = UNet3D(n_in_channels=1, n_out_classes=1, firstdim=48)
+
+
+    # Initialize prediction 2
+    params2 = open_experiment(experiment_name2, global_config_path)
+    cfg_path2 = params2['cfg_path']
+
+    # Generate test set
+    test_dataset = data_loader_3D(cfg_path=cfg_path2, mode='test', modality=modality, multimodal=multimodal2)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=params2['Network']['batch_size_testvlid'],
+                                               pin_memory=True, drop_last=False, shuffle=False, num_workers=16)
+
+    predictor2 = Prediction(cfg_path2)
+    predictor2.setup_model(model=model2, modelepoch=experiment2_epoch_num)
+    test_F1_2, test_accuracy2, test_specificity2, test_sensitivity2, test_precision2 = predictor2.predict_only(test_loader)
+
+    F1_list2, accuracy_list2, specificity_list2, sensitivity_list2, precision_list2 = predictor2.bootstrapper(test_F1_2, test_accuracy2, test_specificity2, test_sensitivity2, test_precision2, index_list)
+
+
+    ########################## P-Values ########################
+    predictor2.pvalue_calculator(F1_list1, accuracy_list1, specificity_list1, sensitivity_list1, precision_list1, params1,
+                      F1_list2, accuracy_list2, specificity_list2, sensitivity_list2, precision_list2, params2)
+    ########################## P-Values ########################
+
+
+
+
 def main_predict_3D(global_config_path="picai-segmentation/config/config.yaml", experiment_name='name', modelepoch=2):
     """Evaluation for all the images using the labels and calculating metrics.
 
@@ -174,7 +244,10 @@ def main_predict_3D(global_config_path="picai-segmentation/config/config.yaml", 
 if __name__ == '__main__':
     global_config_path = "/home/soroosh/Documents/Repositories/picai-segmentation/config/config.yaml"
     # delete_experiment(global_config_path=global_config_path, experiment_name='name_noaugment')
-    # main_train_3D(global_config_path=global_config_path, valid=True, resume=False, augment=True, experiment_name='name_noaugment')
+    # main_train_3D(global_config_path=global_config_path, valid=True, resume=False, augment=True, experiment_name='name_noaugmen2')
 
     # main_evaluate_3D(global_config_path=global_config_path, experiment_name='name_noaugment', modelepoch=2)
-    main_predict_3D(global_config_path=global_config_path, experiment_name='name_noaugment', modelepoch=2)
+    # main_predict_3D(global_config_path=global_config_path, experiment_name='name_noaugment', modelepoch=2)
+
+    main_evaluate_3D_bootstrap_pvalue(global_config_path=global_config_path, experiment_name1='name_noaugment', experiment1_epoch_num=2,
+                                      experiment_name2='name_noaugmen2', experiment2_epoch_num=2, multimodal1=True, multimodal2=True)
